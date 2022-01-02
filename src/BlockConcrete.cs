deb1dee30f12e5004ff38f06cb7b2ad2ed5e121c
l33tmaan <tyler.patin@gmail.com>
2022-01-02 09:26:05 -0800

    Add project files.

---
+++ src/BlockConcrete.cs
@@ -0,0 +1,19 @@
+﻿namespace MoreRoads
+{
+    using Vintagestory.API.Common;
+    using Vintagestory.API.MathTools;
+	public class BlockConcrete : Block, IBlockFlowing
+	{
+		public string Flow { get; set; }
+		public Vec3i FlowNormali { get => null; set {} }
+		public bool IsLava => true;
+
+		public BlockConcrete() : base()
+		{ }
+
+		public override void OnLoaded(ICoreAPI api)
+		{
+			base.OnLoaded(api);
+		}
+	}
+}
---
+++ src/MoreRoads.cs
@@ -0,0 +1,13 @@
+﻿using Vintagestory.API.Common;
+
+namespace MoreRoads
+{
+    public class MoreRoads: ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+            api.RegisterBlockClass("BlockConcrete", typeof(BlockConcrete));
+        }
+    }
+}
