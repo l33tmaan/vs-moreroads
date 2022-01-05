232ada606305a480772d8840ac6ccce2aced4461
l33tmaan <tyler.patin@gmail.com>
2022-01-05 12:10:21 -0800

    Added cube shapes for dried concrete

--- src/BlockConcrete.cs
+++ src/BlockConcrete.cs
@@ -6,6 +6,7 @@
 	{
 		public string Flow { get; set; }
 		public Vec3i FlowNormali { get => null; set {} }
+        public string Height { get; set; }
 		public bool IsLava => true;
 
 		public BlockConcrete() : base()
@@ -14,6 +15,9 @@
 		public override void OnLoaded(ICoreAPI api)
 		{
 			base.OnLoaded(api);
+			Flow = Variant["flow"] is string f ? string.Intern(f) : null; ;
+			FlowNormali = Flow != null ? Cardinal.FromInitial(Flow)?.Normali : null;
+			Height = Variant["height"] is string h ? string.Intern(h) : null; ;
 		}
 	}
 }
